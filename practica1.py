07ea6a569c354df026f8861d1f2ec98446bab7a2
Adrian S <adriansalmar@gmail.com>
2021-03-17 12:42:42 -0700

    crear archivos py y md de practica

---
+++ practica1.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+# Archivo por Adrián Salgado
+
+import random
+
+print(f'Un numero aleatorio cualquiera es {random.random()}')
+print('Pero este es el número dos: {8//4}')
---
+++ practica2.md
@@ -0,0 +1,13 @@
+# Archivo con formato Markdown
+
+## Subtitulo
+
+### Por Adrián Salgado
+
+**Lista:**
+
+1. Uno
+2. Dos
+3. Tres
+
+*También* un [link](README.md)
